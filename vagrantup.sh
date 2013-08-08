f11ad5299004a9562c60a91c648ad5b16f354aeb
Tom Rees <zephod@gmail.com>
2013-08-08 22:09:34 +0100

    vagrantup handles rubygems

--- vagrantup.sh
+++ vagrantup.sh
@@ -7,16 +7,15 @@ PUPPET_DIR=/etc/puppet/
 
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
+$(which apt-get > /dev/null 2>&1)
+FOUND_APT=$?
+$(which yum > /dev/null 2>&1)
+FOUND_YUM=$?
 
 $(which git > /dev/null 2>&1)
 FOUND_GIT=$?
 if [ "$FOUND_GIT" -ne '0' ]; then
   echo 'Attempting to install git.'
-  $(which apt-get > /dev/null 2>&1)
-  FOUND_APT=$?
-  $(which yum > /dev/null 2>&1)
-  FOUND_YUM=$?
-
   if [ "${FOUND_YUM}" -eq '0' ]; then
     yum -q -y makecache
     yum -q -y install git
@@ -30,6 +29,25 @@ if [ "$FOUND_GIT" -ne '0' ]; then
   fi
 else
   echo 'git found.'
+fi
+
+$(which gem > /dev/null 2>&1)
+FOUND_GEM=$?
+if [ "$FOUND_GEM" -ne '0' ]; then
+  echo 'Attempting to install rubygems'
+  if [ "${FOUND_YUM}" -eq '0' ]; then
+    yum -q -y makecache
+    yum -q -y install rubygems
+    echo 'rubygems installed.'
+  elif [ "${FOUND_APT}" -eq '0' ]; then
+    apt-get -q -y update
+    apt-get -q -y install rubygems
+    echo 'rubygems installed.'
+  else
+    echo 'No package installer available. You may need to install rubygems manually.'
+  fi
+else
+  echo 'rubygems found.'
 fi
 
 mkdir -p $PUPPET_DIR
